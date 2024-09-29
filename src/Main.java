2c4c5522383565bea4d406c4c2db45d97a1bd304
Askat Narinbetov <231308@astanait.edu.kz>
2024-09-29 22:29:45 +0500

    All tasks

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,19 @@
 import FifthTask.Button;
 import FifthTask.Light;
 import FifthTask.Switchable;
-
+import FirstTask.User;
+import FirstTask.EmailValidator;
+import SecondTask.Shape;
+import SecondTask.Rectangle;
+import SecondTask.Circle;
+import ThirdTask.Bird;
+import ThirdTask.Flyable;
+import ThirdTask.Penguin;
+import ThirdTask.Sparrow;
+import FourthTask.Eatable;
+import FourthTask.HumanWorker;
+import FourthTask.RobotWorker;
+import FourthTask.Workable;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,6 +21,31 @@ public class Main {
         Button button = new Button(light);
         button.press();
 
+// Main of First Task
+//        User user = new User("Alice", "alice@example.com");
+//        EmailValidator validator = new EmailValidator();
+//
+//        if (validator.isValid(user.getEmail())) {
+//            System.out.println(user.getName() + "'s email is valid.");
+//        } else {
+//            System.out.println(user.getName() + "'s email is invalid.");
+//        }
+
+// Main of Second Task
+//        Shape rectangle = new Rectangle(5, 3);
+//        Shape circle = new Circle(4);
+//
+//        System.out.println("Rectangle area: " + rectangle.area());
+//        System.out.println("Circle area: " + circle.area());
+
+// Main of Third Task
+//        Bird sparrow = new Sparrow();
+//        ((Flyable) sparrow).fly();
+//
+//        Bird penguin = new Penguin();
+//        penguin.eat();
+
+// Main of Fourth Task
 //        Workable humanWorker = new HumanWorker();
 //        Workable robotWorker = new RobotWorker();
 //
